11e354d0d61ae885070d5ca513584b424c0b17c7
Umid <umidjorayev091@gmail.com>
2026-06-06 18:51:18 +0500

    permisson yozildi

--- api/admin/testmedia/views.py
+++ api/admin/testmedia/views.py
@@ -1,10 +1,13 @@
 from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
+
 from apps.testmedia.models import DataModel
 from api.admin.testmedia.serializer import MediaDataSerializer
 
 class MediaDataCreateView(CreateAPIView):
     queryset = DataModel.objects.all()
     serializer_class = MediaDataSerializer
+    permission_classes = [AllowAny]
 
     def perform_create(self, serializer):
         serializer.save(payload=self.request.data)
