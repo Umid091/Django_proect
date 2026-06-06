0b607dcf443e5ae05ac2ded1130eb4c96712f901
Umid <umidjorayev091@gmail.com>
2026-06-06 18:39:06 +0500

    adminga model qoshib qoyildi

--- apps/testmedia/admin.py
+++ apps/testmedia/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import DataModel
 
-# Register your models here.
+@admin.register(DataModel)
+class DataModelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'created_at')
+    search_fields = ('id',)
